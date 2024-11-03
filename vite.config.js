34b448fa630105c5b4914ec70bac2170aa00308b
Scorpion4207 <telitsin.andrei2015@yandex.ru>
2024-11-03 13:37:27 +0700

    Added section header-hero

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,6 @@
 import { defineConfig } from 'vite';
 import injectHTML from 'vite-plugin-html-inject';
-import { ViteMinifyPlugin } from 'vite-plugin-minify'
+import { ViteMinifyPlugin } from 'vite-plugin-minify';
 import { ViteImageOptimizer } from 'vite-plugin-image-optimizer';
 
 export default defineConfig({
